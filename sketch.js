fb860a091e8704b4834afcdc88749bcfda300e6a
JACK SEAMANS <JJS22F@FSU.EDU>
2023-09-11 11:36:08 -0400

    initial commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
 }
 
 function draw() {
-  background(125);
+  background("orange");
+
+  fill("yellow")
+  rect(200, 200, 100, 100);
+
+fill("green")
+  ellipse(30, 30, 30)
+
 }
