4a83de9da5f168079461bf7c671d1fc8f6417287
JACK SEAMANS <JJS22F@FSU.EDU>
2023-09-12 17:34:26 -0400

    Finished Mondrian

--- sketch.js
+++ sketch.js
@@ -4,12 +4,47 @@ function setup() {
 }
 
 function draw() {
-  background("orange");
+  background("white");
 
-  fill("yellow")
+fill("red")
+  rect(10, 40, 500)
+
+fill("blue")
+  rect(275, 250, 200)
+fill("yellow")
+  rect(50, 340, 100)
+
+fill("white")
+  rect(150, 340, 100)
+
+fill("white")  
+  rect(250, 340, 100)
+
+fill("black")
+  rect(20, 310, 40)
+
+fill("blue")
+  rect(180, 310, 40)
+
+fill("black")
+  rect(220, 310, 40)
+
+fill("black")
+  rect(45, 190, 90)
+
+fill("yellow")
   rect(200, 200, 100, 100);
 
-fill("green")
-  ellipse(30, 30, 30)
+fill("white")
+  rect(15, 15, 120)
+
+fill("white")
+  rect(150, 60, 150)
+
+fill("blue")
+  rect(360, 40, 100)
+
+fill("white")
+  rect(275, 40, 100)
 
 }
